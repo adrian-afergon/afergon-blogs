79683e3f12ee7eb9ffb3ef92cc8b3f2117e5b6a8
Adrian Ferrera <adrian.afergon@gmail.com>
2020-05-11 08:54:50 +0100

    Update content

--- pages/index.js
+++ pages/index.js
@@ -3,57 +3,25 @@ import Head from 'next/head'
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>Adrián Ferrera</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        I'm working to give you access to the content early as possible
       </h1>
 
       <p className="description">
-        Get started by editing <code>pages/index.js</code>
+        We will be up soon!!
       </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
     </main>
 
     <footer>
       <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
+        href="mailto:adrian.afergon@gmail.com"
       >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+        Contact with me
       </a>
     </footer>
 

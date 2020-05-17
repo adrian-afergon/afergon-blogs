411e9194eae236bf3321404866897e184e71e423
Adrian Ferrera <adrian.afergon@gmail.com>
2020-05-17 12:27:27 +0100

    Remove non existent style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
-
+import { AppProps } from 'next/app'
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the CSS
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
-
-import '../styles/global.css'
-import { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />

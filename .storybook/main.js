5078db99a36e3fc81240fe1c2d3005d7ea8d8301
Adrian Ferrera <adrian.afergon@gmail.com>
2020-05-24 10:26:57 +0100

    Configure sass

--- .storybook/main.js
+++ .storybook/main.js
@@ -26,6 +26,12 @@ module.exports = {
         },
       ],
     });
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
     config.resolve.extensions.push('.ts', '.tsx');
     return config;
   },
--- next.config.js
+++ next.config.js
@@ -1,2 +1,4 @@
 const withCSS = require('@zeit/next-css');
-module.exports = withCSS({});
+// next.config.js
+const withSass = require('@zeit/next-sass')
+module.exports = withSass(withCSS({}))

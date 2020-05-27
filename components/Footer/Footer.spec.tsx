e835d6f99c781c23f54addbb23e3818861149155
Adrian Ferrera <adrian.afergon@gmail.com>
2020-05-27 23:29:25 +0100

    test: skip unnecessary test right now

--- components/Footer/Footer.spec.tsx
+++ components/Footer/Footer.spec.tsx
@@ -3,7 +3,7 @@ import { render, RenderResult } from '@testing-library/react';
 import { Footer} from './';
 
 describe('Footer', () => {
-  it('should display the default message', () => {
+  xit('should display the default message', () => {
     const renderResult: RenderResult = render(
       <Footer/>,
     );

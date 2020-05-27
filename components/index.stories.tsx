769c70bd9e27570ef3d9dbe485a1a3a914d506a3
Adrian Ferrera <adrian.afergon@gmail.com>
2020-05-27 21:28:21 +0100

    Remove unnecessary story

--- components/index.stories.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export default { title: 'Button' };
-
-export const withText = () => <button>Hello Button</button>;
-
-export const withEmoji = () => <button>Click me please</button>;

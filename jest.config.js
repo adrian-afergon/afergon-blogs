5723676ac2f3181ba11201376d7dc46e9e5b4109
Adrian Ferrera <adrian.afergon@gmail.com>
2020-05-27 22:39:41 +0100

    Configure testing library

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,13 @@ module.exports = {
     verbose: true,
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['./jest.setup.ts'],
     globals: {
         "ts-jest": {
             tsConfig: "tsconfig.jest.json"
         }
+    },
+    "moduleNameMapper": {
+        "^.+\\.(css|less|scss)$": "identity-obj-proxy"
     }
 };

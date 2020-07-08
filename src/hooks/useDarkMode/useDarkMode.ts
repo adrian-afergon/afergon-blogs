35c062f94c7eea532d923681c8000143bc56842a
Adrian Ferrera <adrian.afergon@gmail.com>
2020-07-08 07:54:38 +0100

    feat: handle theme from navigator and localstorage

---
+++ src/hooks/useDarkMode/useDarkMode.ts
@@ -0,0 +1,41 @@
+import { useLayoutEffect, useState } from "react";
+
+interface DarkModeProps {
+  isDarkMode: boolean
+  changeMode: () => void
+}
+
+enum Theme {
+  dark = 'dark',
+  light = 'light'
+}
+
+export const useDarkMode = (): DarkModeProps => {
+
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  const changeThemeAttributes = (theme: string) => {
+    document.documentElement.setAttribute("data-theme", theme)
+    document.documentElement.setAttribute("class", theme)
+    localStorage.setItem('theme', theme)
+  }
+
+  const changeMode = () => {
+    setDarkMode(!isDarkMode);
+    changeThemeAttributes(isDarkMode ? Theme.light : Theme.dark)
+  }
+
+  useLayoutEffect(() => {
+    const savedTheme = localStorage.getItem('theme')
+    if (savedTheme) {
+      setDarkMode(savedTheme === Theme.dark)
+      changeThemeAttributes(savedTheme);
+    } else {
+      const currentDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
+      setDarkMode(currentDarkMode)
+      changeThemeAttributes(currentDarkMode ? Theme.dark : Theme.light);
+    }
+  }, []);
+
+  return { isDarkMode, changeMode }
+}

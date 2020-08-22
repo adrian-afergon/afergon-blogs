dd4c93ffa831414b1a0dbfbf73305f89a2e94dd6
Adrian Ferrera <adrian.afergon@gmail.com>
2020-08-22 14:49:00 +0100

    test: update test

--- src/components/ResourceCard/ResourceCard.spec.tsx
+++ src/components/ResourceCard/ResourceCard.spec.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
 import { render, RenderResult } from '@testing-library/react';
-import { ResourceCard} from './';
+import { ResourceCard} from './ResourceCard';
+import { Resource } from "../../models/resource";
+
+const buildResource = ({
+  date = 'irrelevant date',
+  external = false,
+  image = 'irrelevant url',
+  link = 'irrelevant link',
+  title = 'irrelevant title',
+}: Partial<Resource>): Resource => ({
+  link,
+  date,
+  external,
+  image,
+  title,
+});
 
 describe('ResourceCard', () => {
   it('should display the default message', () => {
+    const resource: Resource = buildResource({});
     const view: RenderResult = render(
-      <ResourceCard/>,
+      <ResourceCard resource={resource}/>,
     );
-    expect(view.queryByText('Hello from ResourceCard!')).toBeTruthy();
+    expect(view.queryByText(resource.title)).toBeInTheDocument();
   });
 });

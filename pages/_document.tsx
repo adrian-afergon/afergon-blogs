0cebb24f694e77e24c7dae55966eee78f5cfde73
Adrian Ferrera <adrian.afergon@gmail.com>
2020-08-30 14:27:43 +0100

    refactor: move google tag manager to a script file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,12 +10,7 @@ export default class MyDocument extends Document {
         <meta name="description" content="Hello and welcome to my website! My name is Adrián Ferrera, I'm a Full Stack Developer that love Typescript and likes to help the developers community. Here you can check my blog, talks, resources or just contact with me." />
         <meta property="og:image" content="/images/profile.jpg" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-171380182-1" />
-        <script dangerouslySetInnerHTML={{__html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'UA-171380182-1');`}} />
+        <script type="text/javascript" src="/gtm.js" />
       </Head>
       <body>
       <Main />

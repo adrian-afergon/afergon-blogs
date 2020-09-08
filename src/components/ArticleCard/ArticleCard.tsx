19a9c60253aaa3edee05f670f4be3b6e13e6889a
Adrian Ferrera <adrian.afergon@gmail.com>
2020-09-08 11:29:05 +0100

    feat: check locales to display the correct component

--- src/components/ArticleCard/ArticleCard.tsx
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -3,6 +3,7 @@ import './ArticleCard.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import { Article } from "../../models/article";
+import { LocaleTag } from "../LocaleTag";
 
 interface ArticleCardProps {
   item: Article
@@ -19,32 +20,38 @@ const formatDate = (date: Date) => {
 }
 
 export const ArticleCard: React.FC<ArticleCardProps> = ({item}) => (
-  <article className="ArticleCard">
-    <section className="card-header">
-      <h2>{item.title}</h2>
-      <span className="date">{formatDate(item.date)}</span>
-    </section>
-    <section className="card-body">
-      <p>{item.intro}</p>
-    </section>
-    <section className="card-footer">
-      <div className="locale">{item.locale}</div>
-      {/*{"video" in item && item.video && (*/}
-      {/*  <div className="external">*/}
-      {/*    <Link href={item.video}>*/}
-      {/*      <a href={item.video} target="_blank">*/}
-      {/*        Watch the video*/}
-      {/*        <FontAwesomeIcon icon={faYoutube} size="xs" aria-label={ArticleCardText.VIDEO_LINK}/>*/}
-      {/*      </a>*/}
-      {/*    </Link>*/}
-      {/*  </div>*/}
-      {/*)}*/}
-      {item.external && (
-        <div className="external">
-          <FontAwesomeIcon icon={faExternalLinkAlt} size="xs" aria-label={ArticleCardText.EXTERNAL_LINK} />
-        </div>
-      )}
+    <article className="ArticleCard">
+      <section className="card-header">
+        <h2>{item.title}</h2>
+        <span className="date">{formatDate(item.date)}</span>
+      </section>
+      <section className="card-body">
+        <p>{item.intro}</p>
+      </section>
+      <section className="card-footer">
+        {
+          item.locales
+            ? item.locales.map(localeResource => <LocaleTag
+              key={localeResource.link}>{localeResource.locale}</LocaleTag>)
+            : <LocaleTag>{item.locale}</LocaleTag>
 
-    </section>
-  </article>
-);
+        }
+        {/*{"video" in item && item.video && (*/}
+        {/*  <div className="external">*/}
+        {/*    <Link href={item.video}>*/}
+        {/*      <a href={item.video} target="_blank">*/}
+        {/*        Watch the video*/}
+        {/*        <FontAwesomeIcon icon={faYoutube} size="xs" aria-label={ArticleCardText.VIDEO_LINK}/>*/}
+        {/*      </a>*/}
+        {/*    </Link>*/}
+        {/*  </div>*/}
+        {/*)}*/}
+        {item.external && (
+          <div className="external">
+            <FontAwesomeIcon icon={faExternalLinkAlt} size="xs" aria-label={ArticleCardText.EXTERNAL_LINK}/>
+          </div>
+        )}
+
+      </section>
+    </article>
+  );

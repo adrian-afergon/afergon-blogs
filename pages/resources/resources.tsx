e25193f9be95403d2e5dbe892d9e754f9211e62c
Adrian Ferrera <adrian.afergon@gmail.com>
2021-02-05 16:02:15 +0000

    fix: remove blank property after update dependency

--- pages/resources/resources.tsx
+++ pages/resources/resources.tsx
@@ -1,34 +1,37 @@
-import * as React from "react";
+import * as React from 'react';
 import './resources.scss';
-import Link from "next/link";
-import { Layout } from "../../src/components/Layout";
-import { RepositoryContext } from "../../src/contexts/repositories.context";
-import { Resource } from "../../src/models/resource";
-import { ResourceCard } from "../../src/components/ResourceCard";
+import Link from 'next/link';
+import { Layout } from '../../src/components/Layout';
+import { RepositoryContext } from '../../src/contexts/repositories.context';
+import { Resource } from '../../src/models/resource';
+import { ResourceCard } from '../../src/components/ResourceCard';
 
 export const Resources = () => {
-
   const [resources, setResources] = React.useState<Resource[]>([]);
-  const {resourcesRepository} = React.useContext(RepositoryContext);
+  const { resourcesRepository } = React.useContext(RepositoryContext);
 
   React.useEffect(() => {
     resourcesRepository.getResources().then(setResources);
   });
 
-  return <Layout>
-    <section className="resources">
-      <ul>
-        {resources && resources.map( resource => <li>
-          <Link href={resource.link} target="_blank">
-          <a href={resource.link} target="_blank"
-             rel="noreferrer">
-          <ResourceCard resource={resource}/>
-          </a>
-          </Link>
-        </li>)}
-      </ul>
-    </section>
-  </Layout>
-}
+  return (
+    <Layout>
+      <section className="resources">
+        <ul>
+          {resources &&
+            resources.map((resource) => (
+              <li key={resource.title}>
+                <Link href={resource.link}>
+                  <a href={resource.link} target="_blank" rel="noreferrer">
+                    <ResourceCard resource={resource} />
+                  </a>
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+};
 
 export default Resources;

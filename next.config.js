3bd37c482b6d79e52cea2287be697e04c77974de
Adrián Ferrera <adrian.ferrera@leanmind.es>
2023-04-15 19:27:00 +0100

    Add configuration to rewrite request from devslives.com to podcast path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+const rewrites =
+[
+    {
+      "source": "/:path*",
+      "has": [
+        {
+          "type": "host",
+          "value": "devslives.com"
+        }
+      ],
+      "destination": "/podcast/:path*"
+    }
+  ]
+
+
 const nextConfig = {
   reactStrictMode: true,
+  rewrites
 }
 
 module.exports = nextConfig

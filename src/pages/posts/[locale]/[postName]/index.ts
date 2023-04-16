38364b482dfc31c2a16d1c3dfa5b2216ac32056b
Adrián Ferrera <adrian.ferrera@leanmind.es>
2023-04-16 18:00:30 +0100

    🔧 Configure redirection for old path

--- src/pages/posts/[locale]/[postName]/index.ts
+++ src/pages/posts/[locale]/[postName]/index.ts
@@ -1,22 +1,12 @@
 import {PostPage} from '../../../../views/post/post'
-import {generateRssFeed} from "@/lib/posts/infrastructure/feed.repository";
 import {PostsFactoryRepository} from "@/lib/posts/infrastructure/posts.factory.repository";
 
 export async function getStaticProps({...ctx}) {
-
-  const [{post, metadata, markdownBody}] = await Promise.all([
-    PostsFactoryRepository.getInstance().getPostFile(ctx.params),
-    //TODO: this is a temporary solution for generation RSS
-    generateRssFeed()
-  ])
-
   return {
-    props: {
-      post,
-      metadata,
-      markdownBody
-    },
-    revalidate: 1
+    redirect: {
+      permanent: true,
+      destination: `/${ctx.params.locale}/blog/${ctx.params.postName}`
+    }
   }
 }
 
@@ -24,7 +14,7 @@ export async function getStaticPaths() {
   const paths = await PostsFactoryRepository.getInstance().getAllFilePaths()
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 
